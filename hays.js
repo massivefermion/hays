6c046f2b1d59c74bb12e6821b2dd667d44e54c7f
shayan javani <shayan.javani@gmail.com>
2021-12-05 21:23:51 +0330

    fixed bug

--- hays.js
+++ hays.js
@@ -41,7 +41,7 @@ function findOne(object, key, opts) {
 
     if (k == key) {
       const temp = { path: k, data: { value: object[k] } }
-      if (opts) {
+      if (object[k] && opts) {
         if (opts.transform && typeof opts.transform == 'function') {
           temp.data.transformed = opts.transform(object[k])
         }
